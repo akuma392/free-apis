bc3b17a2d130e227ac57ce6f8c6983507d0bb37e
Abhishek <abhivision3@gmail.com>
2026-04-06 20:55:23 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ message: "Welcome to Free APIs Backend" });
+  const now = new Date();
+  res.json({ 
+    message: "Welcome to Free APIs,
+    date: now.toDateString(),
+    time: now.toLocaleTimeString()
+    });
 });
 
 module.exports = router;
