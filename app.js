13c68149c78a61d6a460517b743538d95c6042a8
Abhishek <abhivision3@gmail.com>
2026-04-09 22:54:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+// cors
 var cors = require("cors");
 
 require("dotenv").config();
